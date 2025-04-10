cdb8081afb49380cbe25e6dfc08e30cf8613936a
Jason Hankins <tul53850@temple.edu>
2025-04-09 20:41:01 -0400

    Assignment 2 - Add Fuctionality

--- Assignment2/App.js
+++ Assignment2/App.js
@@ -2,9 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 import React, { useState } from 'react';
 
-
-
 export default function App() {
+
+  const [inputText, setInputText] = useState('');
+
+  const [items, setItems] = useState(['Item 1', 'Item 2', 'Item 3']);
+
+  const addItem = () => {
+    if (inputText.trim()) {
+      setItems([...items, inputText]);
+      setInputText('');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <FlatList
@@ -13,15 +23,15 @@ export default function App() {
 
       <TextInput
         //style={styles.input}
-        //value={inputText}
-        //onChangeText={setInputText}
+        value={inputText}
+        onChangeText={setInputText}
         placeholder="Enter new item"
         placeholderTextColor="#999"
       />
 
       <Button 
         title="Add" 
-        //onPress={addItem} 
+        onPress={addItem} 
         color="#007bff"
       />
 
