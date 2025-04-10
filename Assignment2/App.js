fe71828ca2fc051bc5e6d8f5b08895173c8b4404
Jason Hankins <tul53850@temple.edu>
2025-04-09 20:32:50 -0400

    Assignment 2 - UI Items

--- Assignment2/App.js
+++ Assignment2/App.js
@@ -1,46 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import React, { useState } from 'react';
 
-const [inputText, setInputText] = useState('');
 
-const [items, setItems] = useState([
-  'Item 1', 'Item 2', 'Item 3', 'Item 4'
-]);
-
-const addItem = () => {
-  if (inputText.trim() && inputText!='') {
-    setItems([...items, inputText]);
-    setInputText('');
-  }
-};
-
-const renderItem = ({ item }) => (
-  <View style={styles.itemContainer}>
-    <Text style={styles.itemText}>{item}</Text>
-  </View>
-);
 
 export default function App() {
   return (
     <View style={styles.container}>
       <FlatList
-        data={items}
-        renderItem={renderItem}
-        keyExtractor={(item, index) => index.toString()}
         numColumns={2}
       />
 
       <TextInput
-        style={styles.input}
-        value={inputText}
-        onChangeText={setInputText}
+        //style={styles.input}
+        //value={inputText}
+        //onChangeText={setInputText}
         placeholder="Enter new item"
         placeholderTextColor="#999"
       />
 
       <Button 
         title="Add" 
-        onPress={addItem} 
+        //onPress={addItem} 
         color="#007bff"
       />
 
