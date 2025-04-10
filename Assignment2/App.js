f5b914a4b4e04a1fc386da905cf4616da0123073
Jason Hankins <tul53850@temple.edu>
2025-04-09 20:09:13 -0400

    Assignment 2 - InputText

--- Assignment2/App.js
+++ Assignment2/App.js
@@ -1,10 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const [inputText, setInputText] = useState('');
+
+const [items, setItems] = useState([
+  'Item 1', 'Item 2', 'Item 3', 'Item 4'
+]);
+
+const addItem = () => {
+  if (inputText.trim()) {
+    setItems([...items, inputText]);
+    setInputText('');
+  }
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        data={items}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+        numColumns={2}
+        //contentContainerStyle={styles.listContent}
+        //columnWrapperStyle={styles.columnWrapper}
+      />
+
+      <TextInput
+        style={styles.input}
+        value={inputText}
+        onChangeText={setInputText}
+        placeholder="Enter new item"
+        placeholderTextColor="#999"
+      />
+
+      <Button 
+        title="Add" 
+        onPress={addItem} 
+        color="#007bff"
+      />
+
+
       <StatusBar style="auto" />
     </View>
   );
