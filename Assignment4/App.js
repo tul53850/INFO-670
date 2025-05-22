2d8f8be8847229c10971deaa0d9bf98265a96060
Jason Hankins <tul53850@temple.edu>
2025-05-22 18:36:45 -0400

    Assignment 4 - Node.js & Express API Endpoints

--- Assignment4/App.js
+++ Assignment4/App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+
+  const [data, setData] = useState('');
+  useEffect(() => {fetchData();}, []);
+  const fetchData = async() => {
+    try{
+      const response = await axios.get('http://localhost:5000/api/data');
+      setData(response.data.message);
+    } catch (error) {console.log(error);}
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{data}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ Assignment4/server/server.js
@@ -0,0 +1,17 @@
+const express = require('express'); //NODE.JS & EXPRESS
+const bodyParser = require('body-parser'); // JSON body parser
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(bodyParser.json());
+
+app.listen(PORT, () =>{ //API routes here
+    console.log(`Server Running on port ${PORT}`)
+});
+
+app.get('/api/data', (req, res) => { //returns JSON response
+    const data = {
+        message: 'test message mf'
+    };
+    res.json(data);
+});
+//https://www.appsdevpro.com/blog/develop-a-full-stack-react-native-app-using-node-js-backend/
